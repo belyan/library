461c0cd876ef5e5d71d3a016739580105cfac6e3
Yuriy Belyakov <belyan@mail.ru>
2015-07-24 03:15:31 +0300

    Основные утилиты для работы с запросами GET

--- js/flanagan/HTTP.js
+++ js/flanagan/HTTP.js
@@ -32,3 +32,35 @@ HTTP.newRequest = function() {
     };
     HTTP._factory();
 };
+
+/**
+ * Принимает ответ в виде простого текста и передает его указанной функции
+ */
+HTTP.getText = function(url, callback) {
+    var request = HTTP.newRequest();
+
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            callback(request.responseText);
+        }
+    };
+
+    request.open("GET", url);
+    request.send(null);
+};
+
+/**
+ * Принимает XML-документ и передает его функции обратного вызова
+ */
+HTTP.getXML = function(url, callback) {
+    var request = HTTP.newRequest();
+
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            callback(request.responseXML);
+        }
+    };
+
+    request.open("GET", url);
+    request.send(null);
+};
