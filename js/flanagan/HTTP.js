1df73147b8022e669c311ca00a54ad26eb66be5a
Yuriy Belyakov <belyan@mail.ru>
2015-07-25 03:29:49 +0300

    Получение только заголовков

--- js/flanagan/HTTP.js
+++ js/flanagan/HTTP.js
@@ -64,3 +64,50 @@ HTTP.getXML = function(url, callback) {
     request.open("GET", url);
     request.send(null);
 };
+
+/**
+ * Использует HTTP-запрос HEAD для получения заголовков с указанного URL-адреса
+ */
+HTTP.getHeaders = function(url, callback, errorHandler) {
+    var request = HTTP.newRequest();
+
+    request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+            if (request.status == 200) {
+                callback(HTTP.parseHeaders(request));
+            } else {
+                if (errorHandler) {
+                    errorHandler(request.status, request.statusText);
+                } else {
+                    callback(null);
+                }
+            }
+        }
+    };
+
+    request.open("HEAD", url);
+    request.send(null);
+};
+
+/**
+ * Анализирует заголовки ответа, полученные в XMLHttpRequest, и возвращает
+ * имена и значения в виде свойств нового объекта
+ */
+HTTP.parseHeaders = function(request) {
+    var headerText = request.getAllResponseHeaders();
+    var headers = {};
+    var ls = /^\s*/;
+    var ts = /\s*$/;
+
+    var lines = headerText.split("\n");
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        if (line.length == 0) continue;
+        var pos = line.indexOf(":");
+        var name = line.substring(0, pos).replace(ls, "").replace(ts, "");
+        var value = line.substring(pos + 1).replace(ls, "").replace(ts, "");
+        headers[name] = value;
+    }
+
+    return headers;
+};
