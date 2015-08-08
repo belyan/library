cfe8363cee5b520c8adcabe684fab47407149699
Yuriy Belyakov <belyan@mail.ru>
2015-08-09 00:43:39 +0300

    Преобразование XML-документа с помощью XSLT

--- js/flanagan/XML.js
+++ js/flanagan/XML.js
@@ -73,3 +73,49 @@ XML.loadAsync = function(url, callback) {
 
     xmldoc.load(url);
 };
+
+/**
+ * Этот класс XML.Transformer инкапсулирует таблицу XSL-стилей.
+ * Если параметр stylesheet представляет собой URL-адрес, выполняется
+ * загрузка таблицы. Иначе предполагается, что это ссылка
+ * на соответствующий DOM-объект Document.
+ */
+XML.Transformer = function(stylesheet) {
+    if (typeof stylesheet == "string") stylesheet = XML.load(stylesheet);
+    this.stylesheet = stylesheet;
+
+    if (typeof XSLTProcessor != "undefined") {
+        this.processor = new XSLTProcessor();
+        this.processor.importStylesheet(this.stylesheet);
+    }
+};
+
+/**
+ * Это метод transform() класса XML.Transformer.
+ * Выполняет преобразование указанного xml-узла с использованием
+ * инкапсулированной таблицы стилей.
+ * Предполагается, что в результате преобразования получается HTML-код,
+ * которым следует заменить содержимое указанного элемента.
+ */
+XML.Transformer.prototype.transform = function(node, element) {
+    if (typeof element == "string") element = document.getElementById(element);
+
+    if (this.processor) {
+        var fragment = this.processor.transformToFragment(node, document);
+        element.innerHTML = "";
+        element.appendChild(fragment);
+    } else if ("transformNode" in node) {
+        element.innerHTML = node.transformNode(this.stylesheet);
+    } else {
+        throw "XSLT не поддерживается в этом браузере";
+    }
+};
+
+/**
+ * Эта вспомогательная функция, выполняющая XSLT-преобразование,
+ * может быть удобна, когда таблица стилей должна использоваться всего один раз.
+ */
+XML.transform = function(xmldoc, stylesheet, element) {
+    var transformer = new XML.Transformer(stylesheet);
+    transformer.transform(xmldoc, element);
+};
